866a4bca1b2bbdebdd82d3e7f1bb8f4305066615
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-10 15:16:14 +0100

    API added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
+//components
 import { Drawer } from "@material-ui/core";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
+//style
+import { Wrapper } from "./App.styles";
+
+const getProducts = async () => {
+  await (await fetch("https://fakestoreapi.com/products")).json();
+};
+
 const App = () => {
   return <div className="App">hello world</div>;
 };
