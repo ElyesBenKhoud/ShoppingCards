4271ff82c81272b44c4fea696dde35bb48213846
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-10 15:13:27 +0100

    style component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import { Drawer } from "@material-ui/core";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import Badge from "@material-ui/core/Badge";
 const App = () => {
   return <div className="App">hello world</div>;
 };
