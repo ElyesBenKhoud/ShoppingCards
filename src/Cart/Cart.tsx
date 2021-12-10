ed7d6b7b1c9fb7c6b738a3ed71cfdb5757a1646b
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-10 22:24:54 +0100

    cartItem props passed

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -17,7 +17,12 @@ const Cart: React.FC<Props> = ({ CartItems, addToCart, removeFromCart }) => {
       <h2> your shopping cart</h2>
       {CartItems.length === 0 ? <p>No items in cart</p> : null}
       {CartItems.map((item) => (
-        <CartItem />
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
       ))}
     </Wrapper>
   );
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -4,7 +4,11 @@ import { Button } from "@material-ui/core";
 import { CartItemType } from "../App";
 //style
 import { Wrapper } from "./CartItem.styles";
-
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
 const CartItem: React.FC = () => <div> Cart Item</div>;
 
 export default CartItem;
