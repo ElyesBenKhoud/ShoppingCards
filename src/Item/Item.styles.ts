32bdc359af1d67867ae030a559ffe7dc7b3e4783
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-10 15:53:54 +0100

    card style done

--- src/Item/Item.styles.ts
+++ src/Item/Item.styles.ts
@@ -10,12 +10,18 @@ export const Wrapper = styled.div`
   height: 100%;
 
   button {
-      border-radius: 0 0 20px 20px;
-
+    border-radius: 0 0 20px 20px;
   }
 
-  img{
-      max-height: 250px ;
-      object-fit
+  img {
+    max-height: 250px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+  }
+
+  div {
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 1rem;
+    height: 100%;
   }
 `;
--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -2,7 +2,7 @@ import Button from "@material-ui/core/Button";
 //types
 import { CartItemType } from "../App";
 //styles
-import { Wrapper } from "../App.styles";
+import { Wrapper } from "./Item.styles";
 
 type Props = {
   item: CartItemType;
