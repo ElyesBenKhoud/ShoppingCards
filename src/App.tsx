c51dfe5df0496a09f4f33e386fea9494d10ba881
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-10 20:28:59 +0100

    cart component rendering when click on icon cart

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 //components
 import Item from "./Item/Item";
+import Cart from "./Cart/Cart";
 import { Drawer } from "@material-ui/core";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
@@ -43,7 +44,11 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor="right" open={CartOpen} onClose={() => setCartOpen(false)}>
-        cart goes here
+        <Cart
+          CartItems={CartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge color="error" badgeContent={getTotalItems(CartItems)}>
