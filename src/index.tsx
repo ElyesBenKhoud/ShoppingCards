85132275337d19cd1e6e313eef59056c2eefdae3
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-10 15:07:21 +0100

    query react added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+const client = new QueryClient();
 ReactDOM.render(
-  <React.StrictMode>
+  <QueryClientProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </QueryClientProvider>,
   document.getElementById("root")
 );
